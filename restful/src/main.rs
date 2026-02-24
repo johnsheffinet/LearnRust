179373ed260bf71e7b9ef740ae07f03d674fdc0c
John William Sheffield Jr <john@sheffinet.org>
2026-02-24 17:49:31 -0500

    Refactor AppError and AppConfig structures

--- restful/src/main.rs
+++ restful/src/main.rs
@@ -1,23 +1,38 @@
 pub mod handlers {
   pub mod cfg {
-    #[derive(Debug, serde::Deserialize, thiserror::Error)]
+    #[derive(Debug, thiserror::Error)]
     pub enum AppError {
-      #[error("Failed to ...!")]
-      Failed(#[from] ),
+      #[error("Failed to extract environment variables!")]
+      FailedExtractEnvVar(#[from] ),
+      
+      #[error("Failed to validate application configuration!")]
+      FailedValidateAppCfg(#[from] ),
       
     }
 
     pub type AppResult<T> = Result<T, AppError>;
 
-    #[derive()]
+    #[derive(Debug, serde::Deserialize, get_fields::GetFields, validator::Validate)]
     pub struct AppConfig {
       pub http_addr: std::net::SocketAddr;
       pub https_addr: std::net::SocketAddr;
-      pub cert_path: std::PathBuf;
-      pub key_path: std::PathBuf;
+      #[validate(custom(function = "AppConfig::validate_path", message = "Failed to find certificate file!"))]
+      pub cert_path: std::path::PathBuf;
+      #[validate(custom(function = "AppConfig::validate_path", message = "Failed to find key file!"))]
+      pub key_path: std::path::PathBuf;
     }
 
-    impl AppConfig {}
+    impl AppConfig {
+      #[tracing::instrument(err)]
+      fn new() -> AppResult<Self> {
+        
+      }
+
+      #[tracing::instrument(err)]
+      fn validate_path(path: std::path::PathBuf) -> Result<(), validate:ValidationError> {
+        
+      }
+    }
   }
 }
 

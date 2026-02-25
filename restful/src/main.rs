47abe50d3d408ea07c87e9c714aae8038bc98ef9
John William Sheffield Jr <john@sheffinet.org>
2026-02-24 21:53:12 -0500

    Refactor AppConfig and error handling in main.rs

--- restful/src/main.rs
+++ restful/src/main.rs
@@ -1,46 +1,57 @@
 pub mod handlers {
   pub mod cfg {
+    use std::sync::LazyLock;
+    
     #[derive(Debug, thiserror::Error)]
     pub enum AppError {
-      #[error("Failed to extract environment variables!")]
+      #[error("Failed to extract environment variables! {0}")]
       FailedExtractEnvVar(#[from] figment::Error),
       
-      #[error("Failed to validate application configuration!")]
-      FailedValidateAppCfg(#[from] validator::ValidationError),
-      
+      #[error("{0}")]
+      FailedValidate(#[from] validator::ValidationErrors),      
     }
 
     pub type AppResult<T> = Result<T, AppError>;
 
     #[derive(Debug, serde::Deserialize, get_fields::GetFields, validator::Validate)]
+    #[serde(rename_all = "UPPERCASE")]
+    #[get_fields(rename_all = "UPPERCASE")]
     pub struct AppConfig {
-      pub http_addr: std::net::SocketAddr;
-      pub https_addr: std::net::SocketAddr;
+      pub http_addr: std::net::SocketAddr,
+      pub https_addr: std::net::SocketAddr,
       #[validate(custom(function = "AppConfig::validate_path", message = "Failed to find certificate file!"))]
-      pub cert_path: std::path::PathBuf;
+      pub cert_path: std::path::PathBuf,
       #[validate(custom(function = "AppConfig::validate_path", message = "Failed to find key file!"))]
-      pub key_path: std::path::PathBuf;
+      pub key_path: std::path::PathBuf,
     }
 
     impl AppConfig {
       #[tracing::instrument(err)]
-      fn new() -> AppResult<Self> {
-        let config = Figment::new()
+      pub fn new() -> AppResult<Self> {
+        use validator::Validate;
+        
+        let config = figment::Figment::new()
           .merge(figment::providers::Env::raw()
-                .only(Self::getfields)
+                .only(&Self::getfields)
                 .lowercase(false))
-          .extract()?
+          .extract()?;
 
-        config.validate()?
+        config.validate()?;
 
         Ok(config)
       }
 
       #[tracing::instrument(err)]
-      fn validate_path(path: std::path::PathBuf) -> Result<(), validate:ValidationError> {
-        
+      fn validate_path(path: &std::path::PathBuf) -> Result<(), validate:ValidationError> {
+        if path.exists() {
+          Ok(())
+        } else {
+          Err(validator::ValidationError::new("FailedFindFile"))
+        }
       }
     }
+    
+    pub static CONFIG: LazyLock<AppConfig> = LazyLock::new(|| { AppConfig.new().unwrap() })
   }
 }
 

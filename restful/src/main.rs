5430c0efe6dfee6747c607f6a0befc281a69c686
John William Sheffield Jr <john@sheffinet.org>
2026-06-17 19:28:07 +0000

    modified:   restful/src/main.rs

--- restful/src/main.rs
+++ restful/src/main.rs
@@ -181,7 +181,7 @@ pub mod handlers {
     pub async fn check_app_liveliness() -> AppResult<impl IntoResponse> {
         use tokio::time::{Duration, sleep};
 
-        sleep(Duration::from_secs(1)).await;
+        sleep(Duration::from_secs(2)).await;
 
         Ok((StatusCode::OK, Json(json!({"status": "App is lively."}))))
     }
